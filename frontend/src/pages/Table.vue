a3b62497a8ed722213df85e9d7524a612905599b
BlertaKryeziu <bk52307@ubt-uni.net>
2023-03-05 04:01:34 +0100

    table.vue

---
+++ frontend/src/pages/Table.vue
@@ -0,0 +1,29 @@
+<template>
+    <vue-basic-alert :duration="300" :closeIn="2000" ref="alert" />
+    <section class="order-section">
+
+        <div class="heading">
+            <span>book a table</span>
+            <h3>enjoy your moment</h3>
+        </div>
+
+        <div class="icons-container">
+
+            <div class="icons ">
+                <img src="../assets/images/icon-1.png" alt="">
+                <h3>7:00am to 10:00pm</h3>
+            </div>
+
+            <div class="icons">
+                <img src="../assets/images/icon-2.png" alt="">
+                <h3>+84 123 123 123</h3>
+            </div>
+
+            <div class="icons">
+                <img src="../assets/images/icon-3.png" alt="">
+                <h3>02 Duong Khue, Cau Giay, Ha Noi, Viet Nam</h3>
+            </div>
+
+        </div>
+        </section>
+        </template>
