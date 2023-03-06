9a3f449cd67ef929a5233ab7e3bcd1bdfdb51f82
Zeynep Lika <95577898+zeyn13@users.noreply.github.com>
2023-03-06 17:09:32 +0100

    database

---
+++ frontend/src/resources/db_restaurant.sql
@@ -0,0 +1,15 @@
+-- db_restaurant
+
+CREATE TABLE food(
+    food_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    food_name VARCHAR(255),
+    food_star VARCHAR(255),
+    food_vote VARCHAR(255),
+    food_price VARCHAR(255),
+    food_discount VARCHAR(255),
+    food_desc VARCHAR(255),
+    food_status VARCHAR(255),
+    food_type VARCHAR(255),
+    food_category VARCHAR(255),
+    food_src VARCHAR(255)
+)
