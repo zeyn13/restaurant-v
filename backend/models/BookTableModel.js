ce7ae3f7810042af43242ea511bd0969a79d6ca2
Jehona123 <101560137+Jehona123@users.noreply.github.com>
2023-03-07 19:11:19 +0100

    Create BookTableModel.js

---
+++ backend/models/BookTableModel.js
@@ -0,0 +1,16 @@
+// import connection
+import db from " ../config/database.js";
+
+// insert Booking
+export const insertBooking = (data,result) => {
+   db.query("INSERT INTO booktable SET ?",data, (err,results)=> {
+      if (err) {
+         console.log(err);
+         result(err,null);
+      }else{
+         result(null,results[0]);
+      }
+   });
+};  
+  
+        
